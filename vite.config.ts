a87dbee1742bf1e5bcd82d127edb20b44dfac776
micronyks <nikhilshahlive@gmail.com>
2025-02-16 22:13:01 +0530

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteFonts from 'vite-plugin-fonts'
 
-// https://vite.dev/config/
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteFonts({
-      google: {
-        families: ['Source Sans 3', 'Roboto', 'Open Sans'] // Add your desired fonts here
-      }
-    })
   ],
   base: '/rna/', // This will need to be updated with your actual repository name
 })
